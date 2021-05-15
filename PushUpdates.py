48394782ef05140fe3108f7c45da3623e605db88
Mitchell <35324660+Mansalu@users.noreply.github.com>
2021-05-15 13:15:53 -0600

    very primitive request based on changed files

---
+++ PushUpdates.py
@@ -0,0 +1,29 @@
+"""
+Python script used by Git Actions automation to apply changes to SLATE instances
+described by a Git repository.
+"""
+import requests
+import sys
+
+PathToChangedFiles = sys.argv[1]
+slateToken = sys.argv[2]
+
+try:
+    ChangedFiles = open(PathToChangedFiles, 'r').read().split('\n')
+except Exception as e:
+    print("Failed to open temp file", PathToChangedFiles, e)
+
+for FileName in ChangedFiles:
+    containerName = FileName.split('/')[0]
+    instanceDetails = open(containerName + '/' + 'instance.yaml').readlines()
+    clusterName = instanceDetails[0].split(':')[1]
+    groupName = instanceDetails[1].split(':')[1]
+    appName = instanceDetails[2].split(':')[1]
+    valuesString = open(containerName + '/', 'values.yaml').read()
+    response = requests.post('https://api.slateci.io:443/v1alpha3/apps/' + appName, 
+                            params={'token' : slateToken}, 
+                            body={'apiVersion' : 'v1alpha3',
+                                  'group': groupName,
+                                  'cluster': clusterName,
+                                  'configuration': valuesString})
+    print(response)
