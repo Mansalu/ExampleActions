4d3bc8eab3a5681ca8b303155a83e3fc21efbbfb
Mansalu <mitchell.steinman@utah.edu>
2021-06-09 12:41:13 -0600

    fix script

--- PushUpdates.py
+++ PushUpdates.py
@@ -19,7 +19,7 @@ except Exception as e:
     print("Failed to open temp file", PathToChangedFiles, e)
 
 for Entry in ChangedFiles:
-    print(Entry)
+    print(Entry, '\n')
     # Parse entry containing file name and change status
     if (Entry == ''):
         continue
